fe1058913ced202ffc701b37b6e1108352ae529e
NaturesClub <2105891@kiit.ac.in>
2024-08-08 23:55:37 +0530

    feat: route and folder setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
 import './App.css'
-import { Alert } from './components/ui/alert'
-import { Button } from './components/ui/button'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 function App() {
-
   return (
-    <Provider store={store}>
-      <Button>
-        Button
-      </Button>
-      <Alert />
-    </Provider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
