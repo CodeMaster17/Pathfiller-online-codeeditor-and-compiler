ce882e688219add8ffcaa48ab65ae4e168582245
NaturesClub <2105891@kiit.ac.in>
2024-08-08 23:29:00 +0530

    feat: redux added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import './App.css'
 import { Alert } from './components/ui/alert'
 import { Button } from './components/ui/button'
-
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 function App() {
 
   return (
-    <>
+    <Provider store={store}>
       <Button>
         Button
       </Button>
       <Alert />
-    </>
+    </Provider>
   )
 }
 
