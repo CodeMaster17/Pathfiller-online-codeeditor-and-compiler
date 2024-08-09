84ad33caf1666be9f01afac93cb34bba9565b8aa
Harsh <harshku2222@gmail.com>
2024-08-10 01:13:33 +0530

    feat: data fetch from code editor

--- frontend/src/components/core/Problem.tsx
+++ frontend/src/components/core/Problem.tsx
@@ -1,6 +1,7 @@
 import ReactCodeMirror from '@uiw/react-codemirror'
 import { javascript } from '@codemirror/lang-javascript'
 import { vscodeDark } from '@uiw/codemirror-theme-vscode'
+import { useState } from 'react'
 
 import {
     ResizableHandle,
@@ -8,7 +9,14 @@ import {
     ResizablePanelGroup,
   } from "@/components/ui/resizable"
 
+
 const VerticalResizable = () => {
+    let [userCode, setUserCode] = useState<string>();
+
+const onChange = (value:string)=>{
+    console.log(userCode);
+    setUserCode(value);
+}
   return (
     <div className='h-full w-full border'>
       <ResizablePanelGroup
@@ -16,12 +24,13 @@ const VerticalResizable = () => {
       className="min-h-[200px] w-full rounded-lg border"
     >
       <ResizablePanel defaultSize={25}>
-        <div className="flex h-full w-full items-center justify-center p-6">
+        <div className="w-full overflow-auto">
           {/* code-editor */}
             <ReactCodeMirror
                 value='const a = 1;'
                 theme={vscodeDark}
                 extensions={[javascript()]}
+                onChange={onChange}
                 />
         </div>
       </ResizablePanel>
