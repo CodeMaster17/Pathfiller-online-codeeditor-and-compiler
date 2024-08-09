5fc03070064fb60d78005f580058ede884e8131c
Harshit Yadav <2105891@kiit.ac.in>
2024-08-09 22:59:53 +0530

    feat: made route for generating file path

--- express_backend/app.js
+++ express_backend/app.js
@@ -3,15 +3,36 @@ const cors = require('cors')
 const app = express()
 const database = require("./config/database");
 const userRoutes = require("./routes/user");
+const { generateFile } = require('./code/generateFile');
 
 require('dotenv').config()
 app.use(express())
 app.use(cors())
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 database.connect();
 
 app.use("/api/v1/auth", userRoutes);
+
+// ----------------- code -----------------
+// for running the code
+app.post("/run", async (req, res) => {
+    const { language = "cpp", code } = req.body;
+
+    console.log(language, "Length:", code.length);
+
+    if (code === undefined) {
+        return res.status(400).json({ success: false, error: "Empty code body!" });
+    }
+    // need to generate a c++ file with content from the request
+    const filepath = await generateFile(language, code);
+    return res.json({ filepath })
+});
+
+
+// ----------------- code end --------------
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
