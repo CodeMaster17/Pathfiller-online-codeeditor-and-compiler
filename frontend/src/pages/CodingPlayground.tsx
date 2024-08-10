9c8f1d91b32042fa35613e2dbd702416fd542991
Harsh <harshku2222@gmail.com>
2024-08-11 04:58:33 +0530

    codeplayground ui

--- frontend/src/pages/CodingPlayground.tsx
+++ frontend/src/pages/CodingPlayground.tsx
@@ -1,9 +1,49 @@
+import VerticalResizable from "@/components/core/CodingPlayground/VerticalResizable"
+import ReactCodeMirror from '@uiw/react-codemirror';
+import { useState } from "react";
+import { javascript } from '@codemirror/lang-javascript';
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
 
 const CodingPlayground = () => {
+  const [userCode, setUserCode] = useState<string>('');
+  const onChange = (value: string) => {
+    setUserCode(value);
+  };
+  
   return (
-    <div>
-      
+    <div className="">
+      <ResizablePanelGroup
+      direction="horizontal"
+      className="min-h-[calc(100vh-8px)] max-w-full rounded-lg border text-black my-1"
+    >
+      <ResizablePanel defaultSize={70}>
+        <div className="flex flex-col h-full gap-x-5 w-full  rounded-lg ml-auto bg-dark-layer-1 px-3">
+        <div className='text-white items-center font-bold py-2'><span className='text-dark-yellow'>&lt;/&gt;</span> Code</div>
+        <div className="flex h-full w-full items-center justify-center p-1 bg-dark-layer-3">
+        
+        <ReactCodeMirror
+            value={userCode}
+            theme={vscodeDark}
+            extensions={[javascript()]}
+            onChange={onChange}
+            className="w-full h-full"
+          />
+        </div>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={30}>
+        <div className="h-full">
+          <VerticalResizable />
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
     </div>
   )
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -54,6 +54,7 @@ const config = {
         },
         "dark-layer-1": "rgb(40,40,40)",
 				"dark-layer-2": "rgb(26,26,26)",
+        "dark-layer-3": "rgb(30,30,30)",
 				"dark-label-2": "rgba(239, 241, 246, 0.75)",
 				"dark-divider-border-2": "rgb(61, 61, 61)",
 				"dark-fill-2": "hsla(0,0%,100%,.14)",
