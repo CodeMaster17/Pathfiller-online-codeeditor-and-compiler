3607a1a2c73a6631e0d97a42001f3f6a1d40eaea
Harshit Yadav <2105891@kiit.ac.in>
2024-08-29 13:16:19 +0530

    fix: typo

--- express_backend/routes/user.js
+++ express_backend/routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const {
   login,
   signup
-} = require("../controllers/Auth")
+} = require("../controllers/auth")
 
 router.post("/login", login)
 router.post("/signup", signup)
