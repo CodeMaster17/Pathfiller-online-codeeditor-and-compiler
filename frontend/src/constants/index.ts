299c010ff74cb9c6cacb147da744aa5651676cca
Harshit Yadav <2105891@kiit.ac.in>
2024-09-03 22:28:22 +0530

    refactor: constants update

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,11 +1,19 @@
-// export const BACKEND_ROUTE_CODE = "http://localhost:5002/api/v1";
-// export const BACKEND_ROUTE_CODE = "https://pathfiller.kiitians.com";
 export const BACKEND_ROUTE_CODE = "https://pathfiller.kiitians.com/api/v1";
 
-// export const PROBLEM_ROUTE = "http://localhost:5002/api/v1/problem";
-export const PROBLEM_ROUTE =
-  //   "https://pathfiller-web-app-hxh6fqcnbubzfada.eastus-01.azurewebsites.net/api/v1/problem";
-  "https://pathfiller.kiitians.com/api/v1/problem";
+export const PROBLEM_ROUTE = "https://pathfiller.kiitians.com/api/v1/problem";
 
 export const PLAYGROUND_ROUTE =
   "https://pathfiller.kiitians.com/api/v1/playground";
+
+export const CodeBlocksCode = `#include <iostream>
+            using namespace std;
+
+      int main() {
+          bool codeIsPerfect = true;
+          if (codeIsPerfect) {
+              cout << "Code is perfect!" << endl;
+          } else {
+              cout << "Code is not perfect... there are still semicolons to chase." << endl;
+          }
+          return 0;
+      }`;
