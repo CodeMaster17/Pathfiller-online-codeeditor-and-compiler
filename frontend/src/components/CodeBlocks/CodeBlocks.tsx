8d0baa31745b5de9d028f30af400c6b70b6424ed
Harshit Yadav <2105891@kiit.ac.in>
2024-09-04 18:44:57 +0530

    refactor: refactor code for code animation in home page

--- frontend/src/components/CodeBlocks/CodeBlocks.tsx
+++ frontend/src/components/CodeBlocks/CodeBlocks.tsx
@@ -3,39 +3,44 @@ import { CodeBlocksCode } from "@/constants";
 
 
 const CodeBlocks = ({
-  position = "flex-row", // Change to "flex-row" for side-by-side layout
+  position = "flex-row",
   codeblock = CodeBlocksCode,
   codeColor = "text-yellow-400",
 }) => {
   return (
-    <div className={`flex ${position} justify-between gap-10 bg-black p-28 `}>
-
-      <div className="code-border border rounded-lg bg-gray-900 flex flex-row py-16 px-8 text-[10px] sm:text-sm leading-[18px] sm:leading-6 relative w-full lg:w-[470px]">
-        <div className="text-center flex flex-col w-[10%] text-white select-none text-richblack-400 font-inter font-bold">
+    <div className={`flex ${position} justify-between gap-10 bg-black p-28  `}>
+      <div className="code-border border rounded-lg bg-gray-900 flex flex-row py-16 px-2 text-[10px] sm:text-sm leading-[18px] sm:leading-6 relative w-full lg:w-[520px]">
+        <div className="text-center flex flex-col w-[20%] text-white select-none text-richblack-400 font-inter font-bold">
           {[...Array(14).keys()].map((i) => (
             <p key={i}>{i + 1}</p>
           ))}
         </div>
-        <div className={`w-[90%] flex flex-col gap-3 font-bold font-mono ${codeColor} pr-1`}>
+        <div className={`w-[100%] flex flex-col gap-3 font-bold font-mono ${codeColor}`}>
           <TypeAnimation
             sequence={[codeblock, 1000, ""]}
             cursor={true}
             repeat={Infinity}
             style={{
-              whiteSpace: "pre-line",
+              whiteSpace: "pre",
               display: "block",
+              fontFamily: "monospace",
             }}
             omitDeletionAnimation={true}
           />
         </div>
       </div>
 
-      <div className="w-full md:w-1/2 flex flex-col justify-center pl-16">
+      <div className="w-full md:w-1/2 flex flex-col justify-center ">
         <h2 className="text-white text-3xl font-bold mb-4">
-          Empowering Innovation: The Ultimate Coding Platform and Editor for Developers
+          <span className="text-brand-orange">
+            Empowering Innovation: {" "}
+          </span>
+          <br />
+          <br />
+          The Ultimate Coding Platform and Editor for Developers
         </h2>
         <p className="text-gray-300 text-xl pt-4">
-          A coding platform and its editor serve as the heart of a developer's toolkit, offering a powerful environment where ideas are transformed into reality. The best platforms provide a seamless experience, with an intuitive interface that encourages exploration and creativity. A top-tier editor not only simplifies the coding process but also enhances productivity with features like intelligent code completion, real-time collaboration, and a rich library of integrations. It empowers developers to write clean, efficient code while minimizing errors, making it an indispensable resource for both beginners and seasoned professionals. By fostering a community of learners and experts, a great coding platform becomes more than just a tool—it's a catalyst for innovation and growth in the world of technology.
+          A coding platform and editor are essential tools that turn ideas into reality. With intuitive interfaces and features like code completion and collaboration, they boost productivity and foster innovation in technology.
         </p>
       </div>
 
--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -6,14 +6,15 @@ export const PLAYGROUND_ROUTE =
   "https://pathfiller.kiitians.com/api/v1/playground";
 
 export const CodeBlocksCode = `#include <iostream>
-            using namespace std;
+using namespace std;
 
-      int main() {
-          bool codeIsPerfect = true;
-          if (codeIsPerfect) {
-              cout << "Code is perfect!" << endl;
-          } else {
-              cout << "Code is not perfect... there are still semicolons to chase." << endl;
-          }
-          return 0;
-      }`;
+int main() {
+    bool codeIsPerfect = true;
+    if (codeIsPerfect) {
+        cout << "Code is perfect!" << endl;
+    } else {
+        cout << "There are still semicolons to chase."
+        << endl;
+    }
+    return 0;
+}`;
