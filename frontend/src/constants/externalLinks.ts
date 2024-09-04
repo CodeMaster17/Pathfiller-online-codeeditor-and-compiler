c54f6a8e461ca26619db8a6b35820277c1711341
Harshit Yadav <2105891@kiit.ac.in>
2024-09-04 18:58:53 +0530

    refactor: footer update with links and icons

---
+++ frontend/src/constants/externalLinks.ts
@@ -0,0 +1,6 @@
+export const DEVELOPER_GITHUB_LINK = "https://github.com/CodeMaster17";
+export const DEVELOPER_LINKEDIN_LINK =
+  "https://www.linkedin.com/in/harshit-yadav-2b2b2922b/";
+export const DEVELOPER_INSTAGRAM_LINK =
+  "https://www.instagram.com/harshit._._.yadav/";
+export const DEVELOPER_TWITTER_LINK = "https://x.com/coder_harshit";
