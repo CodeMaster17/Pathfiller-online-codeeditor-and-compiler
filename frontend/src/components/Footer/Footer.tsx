4f9bd69ac7e67d9c2eb86ab4f882f04e82e69662
Harshit Yadav <2105891@kiit.ac.in>
2024-12-30 01:07:02 +0530

    feat: Footer created

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,62 +1,34 @@
+import { DEVELOPER_INSTAGRAM_LINK, DEVELOPER_LINKEDIN_LINK, DEVELOPER_TWITTER_LINK } from "@/constants/externalLinks";
+import Logo from "../Logo";
 
-const socials = [
-  {
-    id: "0",
-    title: "x",
-    icon: "/images/socials/x.svg",
-    url: "#",
-  },
-  {
-    id: "1",
-    title: "Threads",
-    icon: "/images/socials/threads.svg",
-    url: "#",
-  },
-  {
-    id: "2",
-    title: "Instagram",
-    icon: "/images/socials/instagram.svg",
-    url: "#",
-  },
-  {
-    id: "3",
-    title: "Discord",
-    icon: "/images/socials/discord.svg",
-    url: "#",
-  },
-];
 
 const Footer = () => {
   return (
-    <footer className="bg-s1">
-      <div className="container py-10 bg-s1">
-        <div className="flex w-full max-md:flex-col">
-          <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
-            <p className="opacity-70 text-p5">Copyright, Harshit Yadav</p>
-          </div>
-          <div className="flex items-center justify-center sm:ml-auto">
-            <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
-              Privacy Policy
-            </p>
-            <p className="text-p5 transition-all duration-500 hover:text-p1">
-              Terms of Use
-            </p>
-          </div>
-
-          <ul className="flex flex-1 justify-center gap-3 max-md:mt-10 md:justify-end">
-            {socials.map(({ id, url, icon, title }) => (
-              <li key={id}>
-                <a href={url} className="social-icon">
-                  <img
-                    src={icon}
-                    alt={title}
-                    className="size-4 object-contain"
-                  />
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <footer className="text-gray-600 body-font">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <Logo variant="small" />
+        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024 Pathfiller —
+          <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@CodeMaster17</a>
+        </p>
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <a href={DEVELOPER_TWITTER_LINK} target="_blank" className="ml-3 text-gray-500">
+            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+              <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+            </svg>
+          </a>
+          <a href={DEVELOPER_INSTAGRAM_LINK} className="ml-3 text-gray-500">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+            </svg>
+          </a>
+          <a href={DEVELOPER_LINKEDIN_LINK} className="ml-3 text-gray-500">
+            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
+              <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+              <circle cx="4" cy="4" r="2" stroke="none"></circle>
+            </svg>
+          </a>
+        </span>
       </div>
     </footer>
   );
